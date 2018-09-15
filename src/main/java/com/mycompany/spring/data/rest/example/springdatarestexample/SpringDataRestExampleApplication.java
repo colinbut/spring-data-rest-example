4b6d2edc547e93c808a60aa5afaf0d43d0a310f5
Colin But <colinbut@users.noreply.github.com>
2018-09-15 15:17:06 +0100

    Project Tenplate generated from Spring Iniitalizer

---
+++ src/main/java/com/mycompany/spring/data/rest/example/springdatarestexample/SpringDataRestExampleApplication.java
@@ -0,0 +1,12 @@
+package com.mycompany.spring.data.rest.example.springdatarestexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRestExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataRestExampleApplication.class, args);
+	}
+}
